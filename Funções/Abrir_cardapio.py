3efe2c855bd946c00ce62b52a148b0b08187dcf9
Iarley <pinheiroiarley985@gmail.com>
2026-07-14 15:57:04 -0300

    Update Abrir_cardapio.py

--- Funções/Abrir_cardapio.py
+++ Funções/Abrir_cardapio.py
@@ -13,7 +13,6 @@ def abrir_cardapio(cliente):
     cardapio.geometry("800x800")
     cardapio.minsize(400, 300)
     cardapio.configure(bg="Brown",  bd= 10, relief="sunken")
-<<<<<<< Updated upstream
 
     # Canvas
     canvas = tk.Canvas(cardapio, bg="Brown", highlightthickness=0)
@@ -36,10 +35,8 @@ def abrir_cardapio(cliente):
 )
 
     acoes = tk.Frame(conteudo, bg="Red")
-=======
    
     acoes = tk.Frame(cardapio, bg="Red")
->>>>>>> Stashed changes
     acoes.pack(pady= 20)
 
     inicio = tk.Frame(conteudo)
